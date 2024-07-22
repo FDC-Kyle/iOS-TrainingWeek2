09dd8baa953676f2e0819e32365d7d6064422fe5
FDC-Kyle <fdc.kylev@gmail.com>
2024-07-22 10:03:07 +0800

    Initial Commit

---
+++ Week2-IOS-Training/Week2_IOS_TrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week2_IOS_TrainingApp.swift
+//  Week2-IOS-Training
+//
+//  Created by FDC-KYLE-NC-WEB on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Week2_IOS_TrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
